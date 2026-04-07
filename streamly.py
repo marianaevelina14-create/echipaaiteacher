c8a412078e3419be559bb0b58ca2b52a1759a096
marianaevelina14-create <marianaevelina14@gmail.com>
2026-04-07 11:12:35 +0300

    Update streamly.py

--- streamly.py
+++ streamly.py
@@ -83,11 +83,6 @@ def is_apology(text):
         "mă ierți"
     ]
     return any(word in text.lower() for word in apologies)
-    supabase.table("messages").insert({
-        "session_id": session_id,
-        "role": role,
-        "content": content
-    }).execute()
     from datetime import datetime, timedelta
 
 def block_chat(session_id):
@@ -99,29 +94,9 @@ def block_chat(session_id):
     }).execute()
 
 
-def is_chat_blocked(session_id):
-    response = supabase.table("chat_limits") \
-        .select("*") \
-        .eq("session_id", session_id) \
-        .execute()
-
-    if response.data:
-        blocked_until = response.data[0]["blocked_until"]
-        blocked_until = datetime.fromisoformat(blocked_until)
-
-        if datetime.utcnow() < blocked_until:
-            return True
-
-    return False
 
 
-def is_inappropriate(text):
-    bad_words = ["prost", "idiot", "stupid", "dracu"]
 
-    for word in bad_words:
-        if word in text.lower():
-            return True
-    return False
 # Streamlit Page Configuration
 def is_educational(text):
     keywords = ["matematica", "istorie", "romana", "explica", "ajuta", "problema"]
